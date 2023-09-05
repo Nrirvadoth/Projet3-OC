c8e62c0624a36d539aecf906f352375942924d8a
Mat <mathurin.malandain+github@gmail.com>
2023-09-05 16:00:42 +0200

    division fonction filters

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -6,6 +6,7 @@ let categories = await categoriesJson.json();
 
 //initialisation page
 generateFilters(categories);
+addFiltersListener();
 generateGallery(gallery);
 
 
@@ -13,35 +14,54 @@ generateGallery(gallery);
 // génération des filtres catégories
 function generateFilters(categories) {
 
-    const filters = document.querySelector(".filters");
+    const filtersContainer = document.querySelector(".filters");
 
     const filterList = document.createElement("ul");
     const filterAll = document.createElement("li");
+
     filterAll.innerText = "Tous";
-    filterAll.classList.add("active");
 
-    filterAll.addEventListener("click", () => {
-        document.querySelector(".gallery").innerHTML = "";
-        generateGallery(gallery);
-    });
-
-    filters.appendChild(filterList);
+    filtersContainer.appendChild(filterList);
     filterList.appendChild(filterAll);
 
     for (let i = 0; i < categories.length; i++) {
         
         const filterItem = document.createElement("li");
-
-        filterItem.addEventListener("click", () => {
-            const galleryFiltered = gallery.filter((galleryElement) => galleryElement.categoryId === categories[i].id);
-            document.querySelector(".gallery").innerHTML = "";
-            generateGallery(galleryFiltered)
-        });
-
+      
         filterItem.innerText = categories[i].name;
         filterList.appendChild(filterItem);
     };
 };
+
+// eventslisteners filtres
+
+function addFiltersListener() {
+    const filters = document.querySelectorAll(".filters ul li");
+
+    filters[0].classList.add("active");
+
+    for (let i = 0; i < filters.length; i++) {
+        filters[i].addEventListener("click", () => {
+            for (let j = 0; j < filters.length; j++) {
+                filters[j].classList.remove("active");
+            }
+
+            if (i === 0) {        
+                document.querySelector(".gallery").innerHTML = "";
+                generateGallery(gallery)
+            } else {
+            
+            const galleryFiltered = gallery.filter((galleryElement) => galleryElement.categoryId === categories[i-1].id);
+
+            document.querySelector(".gallery").innerHTML = "";
+            generateGallery(galleryFiltered)
+            }
+
+            filters[i].classList.add("active");
+        });
+    };
+};
+
 
 // génération de la galerie
 function generateGallery(gallery) {
